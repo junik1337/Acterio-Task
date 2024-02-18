c64d7c6a0cba2b3d0252917343b4e258c04ed74a
junik1337 <abderrachidyassir@gmail.com>
2024-02-18 16:18:35 +0100

    Fix: Max number of likes and title size

--- src/app/components/PostCard.tsx
+++ src/app/components/PostCard.tsx
@@ -23,7 +23,9 @@ const PostCard = ({ data, setter }: props) => {
 		<div className="parent">
 			<div className="card">
 				<div className="content-box">
-					<span className="card-title">{data.title}</span>
+					<span className="card-title w-full flex ">
+						{data.title.length > 50 ? data.title.substring(0, 50) + "..." : data.title}
+					</span>
 					<p className="card-content">
 						{data.body.substring(0, 100)}...
 					</p>
@@ -70,7 +72,7 @@ const PostCard = ({ data, setter }: props) => {
 						<svg fill="white" className='text-[12px]' viewBox="0 0 512 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"></path></svg>
 					</span>
 					<span className="likeCount">
-						{Reactions}
+						{Reactions > 100 ? "100+" : Reactions}
 					</span>
 				</button>
 				<button className="buttohover" onClick={deletePost}>
